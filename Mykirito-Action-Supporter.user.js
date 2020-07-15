e144027c2d55989aa3cbb6479231c24c850cc1c3
REX-PC <tp6ru8bj6@gmail.com>
2020-07-15 21:38:37 +0800

    Update

--- Mykirito-Action-Supporter.user.js
+++ Mykirito-Action-Supporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mykirito 純行動手練輔助器
 // @namespace    http://tampermonkey.net/
-// @version      3.4.0.0
+// @version      4.4.2.1
 // @description  防止手殘
 // @author       ChaosOp
 // @match        https://mykirito.com/*
