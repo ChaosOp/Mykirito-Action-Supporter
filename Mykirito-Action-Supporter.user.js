7ef1d1cafeddafd22a7e7edc9b36f61e621c2a58
REX-PC <tp6ru8bj6@gmail.com>
2020-07-16 23:08:53 +0800

    Bug fixed

--- Mykirito-Action-Supporter.user.js
+++ Mykirito-Action-Supporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mykirito 純行動手練輔助器
 // @namespace    http://tampermonkey.net/
-// @version      15.7.8.6
+// @version      15.7.8.7
 // @description  防止手殘
 // @author       ChaosOp
 // @match        https://mykirito.com/*
@@ -71,7 +71,7 @@ async function action_ready() {
   if (!document.getElementById("exp_total")&!document.getElementById("action_select")) add_action_count_bar();
   edit_exp_bar();
   get_total_exp();
-  add_menu();
+  // add_menu();
 
 }
 
